6e12dc542afb8527ecb7a26a9ebb3e8cdbbf8295
DavidPickles <davidkpickles@gmail.com>
2023-12-02 09:29:03 +0000

    logs version

--- lab.js
+++ lab.js
@@ -1,11 +1,34 @@
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import { existsSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import dotenv from 'dotenv'
 
-const usage = () => console.log("Usage: node ./app.js <config dir>")
+console.log('OIDC-Lab version:', getVersion())
+if (process.argv.length !== 3) {
+   usage()
+   process.exit(0)
+}
+if (process.argv[2] === '-v' || process.argv[2] === '--version') {
+    process.exit(0)
+}
+run({ configDirName: process.argv[2] })
 
-const getMode = () => {
+
+async function run({configDirName})  {
+    const configPath = path.join(
+        dirname(fileURLToPath(import.meta.url)), './configs', configDirName
+    )
+    if (!existsSync(configPath)) {
+        console.error(`No directory: ${configPath}`)
+        usage()
+        return
+    }
+    dotenv.config({ path: `${configPath}/.env`})   
+    const mode = getMode()
+    await import(`./src/${mode}.js`)
+}
+
+function getMode() {
     if (process.env.MODE) {
         if (process.env.MODE !== 'm2m' && mode !== 'oidc') {
             throw new Error("MODE must be 'm2m' or 'oidc'")        
@@ -19,23 +42,14 @@ const getMode = () => {
     } 
 } 
 
-
-if (process.argv.length !== 3) {
-   usage()
-   process.exit(0)
-}
-
-const run = async ({configDirName}) => {
-    const configPath = path.join(
-        dirname(fileURLToPath(import.meta.url)), './configs', configDirName
-    )
-    if (!existsSync(configPath)) {
-        console.error(`No directory: ${configPath}`)
-        usage()
-        return
+function getVersion() {
+    try {
+      return JSON.parse(readFileSync('./package.json')).version
+    } catch (err) {
+      console.error('Unable to read package.json:', err);
     }
-    dotenv.config({ path: `${configPath}/.env`})   
-    const mode = getMode()
-    await import(`./src/${mode}.js`)
 }
-run({ configDirName: process.argv[2] })
+
+function usage() {
+    console.log("Usage: node ./app.js <config dir>")
+}
